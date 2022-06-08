fc903d68bbe0cec41fa46d3bdcd67dc4ddc23e29
MicroverseNelson <poetpenton@gmail.com>
2022-06-08 10:35:05 +0300

    linters refactoring

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 let listSection = document.querySelector('.booklist');
 let addSection = document.querySelector('.addnew');
 let contactSection = document.querySelector('.contacts');
-
-
 let listMenuLink = document.querySelector('#bookslist');
 let addMenuLink = document.querySelector('#addbooks');
 let contactMenuLink = document.querySelector('#contact');
@@ -10,22 +8,21 @@ let contactMenuLink = document.querySelector('#contact');
 document.querySelectorAll('.navbar').forEach((link) => {
   link.addEventListener('click', () => {
     listMenuLink.addEventListener('click', () => {
-      listSection.style.display='block';
+      listSection.style.display= 'block';
       addSection.style.display='none';
-      contactSection.style.display='none';
+      contactSection.style.display= 'none';
     });
-    
+
     addMenuLink.addEventListener('click', () => {
-        listSection.style.display='none';
-        addSection.style.display='block';
-        contactSection.style.display='none';
-      
+        listSection.style.display= 'none';
+        addSection.style.display= 'block';
+        contactSection.style.display= 'none';
     });
-    
+
     contactMenuLink.addEventListener('click', () => {
-      listSection.style.display='none';
-      addSection.style.display='none';
-      contactSection.style.display='block';
+      listSection.style.display= 'none';
+      addSection.style.display= 'none';
+      contactSection.style.display= 'block';
     });
 });
 });
